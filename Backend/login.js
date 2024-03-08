ac9f0309915154c8487fa160dcf07990929df11e
rithik-perera <rithik.perera.2003@gmail.com>
2024-03-08 20:11:04 +0530

    Added new attribute profile, New put method change password

--- Backend/login.js
+++ Backend/login.js
@@ -20,6 +20,7 @@ const userSchema = new mongoose.Schema({
     username: { type: String, required: true },
     password: { type: String, required: true },
     salt: { type: String, required: true }, // Salt field added
+    profilePic: { type: String, default: null },
     name: {
       firstName: { type: String },
       lastName: { type: String }
@@ -91,6 +92,71 @@ app.post('/login', async (req, res) => {
     res.status(500).json({ error: 'Internal Server Error' });
   }
 });
+
+// PUT /change-password route
+app.put('/change-password', async (req, res) => {
+  const { username, oldPassword, newPassword } = req.body;
+
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Hash the input old password with the retrieved salt
+    const hashedOldPassword = hashPassword(oldPassword, user.salt);
+
+    // Compare the hashed old password with the stored hashed password
+    if (hashedOldPassword !== user.password) {
+      return res.status(401).json({ error: 'Invalid old password' });
+    }
+
+    // Generate a new salt and hash the new password
+    const newSalt = crypto.randomBytes(16).toString('hex');
+    const hashedNewPassword = hashPassword(newPassword, newSalt);
+
+    // Update the user's password and salt
+    user.password = hashedNewPassword;
+    user.salt = newSalt;
+
+    // Save the updated user to the database
+    await user.save();
+
+    res.status(200).json({ message: 'Password changed successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// PUT /update-profile-pic route
+app.put('/update-profile-pic', async (req, res) => {
+  const { username, profilePic } = req.body;
+
+  try {
+    // Find the user by username
+    const user = await User.findOne({ username });
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    // Update the profile pic field
+    user.profilePic = profilePic;
+
+    // Save the updated user to the database
+    await user.save();
+
+    res.status(200).json({ message: 'Profile picture updated successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
 
 
 
