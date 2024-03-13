68ece783ced63bd8d2c983aea6baa095c5c4b412
dilmi214 <dilmifernando214@gmail.com>
2024-03-13 22:35:50 +0530

    Revert "Updated the refractor, made game details modal"
    This reverts commit 833d6529cb72156b10cc1479be557dbec18d9bd3.

--- Frontend/TrekReact/screens/GameScreens/MainGameUI.js
+++ Frontend/TrekReact/screens/GameScreens/MainGameUI.js
@@ -1,28 +1,19 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
 import MapView, { PROVIDER_GOOGLE, Marker } from 'react-native-maps';
 import { useNavigation } from '@react-navigation/native';
 import * as Location from 'expo-location';
-import GameLocationModal from './GameLocatonModal';
+
 
 const GameMapScreen = ({ route }) => {
   const navigation = useNavigation();
-  const { finalDestinationList } = route.params;
-  const [selectedLocation, setSelectedLocation] = useState(null);
+  const { selectedPlacesIds, confirmedStarterLocation } = route.params;
 
-  const handleBack = () => {
-    navigation.goBack();
-  };
+  console.log('Details of the first two array items:');
+  console.log('Location 1 - Name', selectedPlacesIds[0].name, 'Longitude:', selectedPlacesIds[0].longitude, 'Latitude:', selectedPlacesIds[0].latitude, 'PlaceID', selectedPlacesIds[0].place_id,);
+  console.log('Location 2 - Name', selectedPlacesIds[1].name, 'Longitude:',  selectedPlacesIds[1].longitude, 'Latitude:', selectedPlacesIds[1].latitude, 'PlaceId', selectedPlacesIds[1].place_id);
 
-  const handlePinPress = (destination) => {
-     setSelectedLocation(destination);
-     console.log('Selected Location:', destination);
-  };
-
-  const closeModal = () => {
-    setSelectedLocation(null);
-  };
-
+  //function to get current location
   const getUserCurrentLocation = async () => {
     try {
       const location = await Location.getCurrentPositionAsync({});
@@ -37,32 +28,32 @@ const GameMapScreen = ({ route }) => {
       // Handle error
     }
   };
-
+  
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE}
         style={styles.map}
         initialRegion={{
-          latitude: finalDestinationList[0].latitude,
-          longitude: finalDestinationList[0].longitude,
+          latitude: confirmedStarterLocation.latitude,
+          longitude: confirmedStarterLocation.longitude,
           latitudeDelta: 0.02,
           longitudeDelta: 0.02,
         }}
         mapType="standard"
       >
-        {/* Marker for the Start Location */}
+        {/* Marker for the start location */}
         <Marker
           coordinate={{
-            latitude: finalDestinationList[0].latitude,
-            longitude: finalDestinationList[0].longitude,
+            latitude: confirmedStarterLocation.latitude,
+            longitude: confirmedStarterLocation.longitude,
           }}
           title="Start Location"
           pinColor="blue" 
         />
 
-        {/* Markers for all destinations except the first one */}
-        {finalDestinationList.slice(1).map((destination, index) => (
+        {/* Markers for selected places */}
+        {selectedPlacesIds.map((destination, index) => (
           <Marker
             key={destination.place_id}
             coordinate={{
@@ -71,19 +62,9 @@ const GameMapScreen = ({ route }) => {
             }}
             title={destination.name}
             pinColor="red"
-            onPress={() => handlePinPress(destination)} // Call handlePinPress function with the clicked destination
           />
         ))}
       </MapView>
-      
-      <TouchableOpacity style={styles.backButton} onPress={handleBack}>
-        <Text style={styles.backButtonText}>Back</Text>
-      </TouchableOpacity>
-
-      {/* Location modal - render only when a pin is selected */}
-      {selectedLocation && (
-        <GameLocationModal isVisible={true} locations={finalDestinationList} clickedLocation={selectedLocation} onClose={closeModal} />
-      )}
     </View>
   );
 };
@@ -97,18 +78,6 @@ const styles = StyleSheet.create({
   map: {
     width: '100%',
     height: '100%',
-  },
-  backButton: {
-    position: 'absolute',
-    top: 50,
-    left: 20,
-    backgroundColor: 'white',
-    padding: 10,
-    borderRadius: 5,
-    zIndex: 999,
-  },
-  backButtonText: {
-    fontWeight: 'bold',
   },
 });
 
