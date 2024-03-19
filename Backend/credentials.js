eb9a9ac4a61576c4152665684c9b875fc134ac2c
rithik-perera <rithik.perera.2003@gmail.com>
2024-03-19 20:02:56 +0530

    deleteing and starting from fresh

--- Backend/credentials.js
+++
@@ -1,8 +0,0 @@
-//credentilas
-const myEmailUsername = 'rithik.perera.2003@gmail.com';
-const myEmailpassword = 'tqzw owki bndf hjlz';
-const databaseLink = "mongodb+srv://rithik20222011:Harpyeagle12345@cluster0.my4g36v.mongodb.net/";
-
-
-// Export the credentials
-module.exports = { myEmailUsername, myEmailpassword, databaseLink};
