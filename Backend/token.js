5917ce5e620f9b1dccfea2916d4a7402518f9aeb
rithik-perera <rithik.perera.2003@gmail.com>
2024-03-20 09:08:52 +0530

    some changes

--- Backend/token.js
+++ Backend/token.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
+const uuidv4 = require('uuid');
 
 const tokenSchema = new mongoose.Schema({
     userId: {type: mongoose.Schema.Types.ObjectId,required: true },
     username: {type: String,required: true},
-    tokenId: {type: String,required: true,unique: true},
+    tokenId: {type: String, default: uuidv4, required: true },
     tokenName: {type: String,required: true},
     tokenLevel: {type: String,enum: ['bronze', 'silver', 'gold'],required: true},
     tokenImageReference: {type: String,required: true},
