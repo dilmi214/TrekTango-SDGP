7b87a389808dfeff0dabf7302c10eb5082136d05
dilmi214 <dilmifernando214@gmail.com>
2024-03-22 14:01:44 +0530

    Revert "Revert "Merge branch 'TestingMerge' of https://github.com/dilmi214/TrekTango-SDGP into TestingMerge""
    This reverts commit 5582772d61ca6213a96e996f47672a071c2352c6.

---
+++ Server/models/socialMediaSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const { v4: uuidv4 } = require('uuid');
+
+const socialMediaSchema = new mongoose.Schema({
+    username: { type: String, required: true },
+    userID: { type: String, required: true },
+    lastRefreshedAt: { type: Date, default: Date.now },
+    posts: [{
+      postId: { type: String, default: uuidv4(), required: true, unique: true },
+      placeId: { type: String, required: true },
+      imageReferenceId: { type: String },
+      uploadToMedia: { type: Boolean, default: false },
+      caption: { type: String },
+      comments: {
+        type: [{
+          commentID: { type: String, default: uuidv4(), required: true, unique: true },
+          username: { type: String, required: true },
+          comment: { type: String, required: true }
+        }],
+        default: [] // Default value set to an empty array
+      },
+      likes: [{ type: String }],
+      createdAt: { type: Date, default: Date.now }
+    }]
+  });
+
+module.exports = mongoose.model('SocialMedia', socialMediaSchema);  
