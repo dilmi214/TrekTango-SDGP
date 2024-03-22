ed4c5281eebb7d322de7bed182b3a20387cbf7b5
rithik-perera <rithik.perera.2003@gmail.com>
2024-03-22 05:55:12 +0530

    cloudinary trila

---
+++ Backend/cloud.js
@@ -0,0 +1,13 @@
+const cloudinary = require('cloudinary').v2;
+
+
+// CLOUDINARY_URL=cloudinary://API_KEY:API_SECRET@CLOUD_NAME
+
+// Return "https" URLs by setting secure: true
+cloudinary.config({
+    cloud_name:'dx3ivul7s',
+    api_key: '299365965126531',
+    api_secret: 'znWwCr2V4f4cZ6uznDrqA_bfZWA',
+    secure: true
+  });
+  
