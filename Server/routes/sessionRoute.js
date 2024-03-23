459fa933a894a11298cfa28eb4fdaa168c0245ac
prajoa2803 <prajoathilagan@gmail.com>
2024-03-23 11:32:51 +0530

    sessionRouter updated

--- Server/routes/sessionRoute.js
+++ Server/routes/sessionRoute.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.router;
+const router = express.Router();
 const {createSession, latestIncompleteSession} = require('../controllers/sessionController');
 
 router.route('./createSession').post(createSession);
